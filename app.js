222d5160d0aae3179fd0455aab4906f72306997c
Admin <sorokivskyi777@gmail.com>
2023-03-11 17:48:18 +0200

    lesson2 learning

--- app.js
+++ app.js
@@ -2,6 +2,45 @@ let express = require('express');
 
 let app = express();
 
+const users = [
+    {
+        name: 'Andriy',
+        age: 34,
+        gender: 'male',
+    },
+    {
+        name: 'Ivan',
+        age: 24,
+        gender: 'male',
+    },
+    {
+        name: 'Artem',
+        age: 23,
+        gender: 'male',
+    },
+    {
+        name: 'Anna',
+        age: 19,
+        gender: 'female',
+    },
+    {
+        name: 'Lilya',
+        age: 14,
+        gender: 'female',
+    },
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+
+    let user = users[+userId - 1];
+
+    res.json(user);
+});
 
 app.get('/welcome', (req, res) => {
     res.send('WELCOME!!!!!!!!!!!!!!!!!!!')

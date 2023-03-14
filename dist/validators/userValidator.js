d92cf3ab6a141b84e28eccaaa7deb920638a62fc
Admin <sorokivskyi777@gmail.com>
2023-03-14 18:51:58 +0200

    lesson 4 hw learning2

---
+++ dist/validators/userValidator.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserValidator = void 0;
+class UserValidator {
+}
+exports.UserValidator = UserValidator;
--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -6,6 +6,6 @@ export interface IMessage {
   message: string;
 }
 
-export interface ICommonResponse<T> extends IMessage{
+export interface ICommonResponse<T> extends IMessage {
   data: T;
 }

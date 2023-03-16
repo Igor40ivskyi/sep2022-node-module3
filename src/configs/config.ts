e03f9589970af6527d846cafde533d2fb8787903
Admin <sorokivskyi777@gmail.com>
2023-03-16 18:41:08 +0200

    lesson 5 learning2 part1 register

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,5 +4,5 @@ config();
 
 export const configs = {
   PORT: process.env.PORT || 5001,
-  DB_URL: process.env.DB_URL || "skdfjlsdjfldsfj",
+  DB_URL: process.env.DB_URL || "skdfjlsdjfldsfjdfgfdg",
 };

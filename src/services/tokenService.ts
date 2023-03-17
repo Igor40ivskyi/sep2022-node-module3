2a89d546fab52b34f9aa6f38f64f15a59f18b853
Admin <sorokivskyi777@gmail.com>
2023-03-17 13:40:09 +0200

    lesson 5 learning2 part2 login2

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,13 +1,14 @@
 import * as jwt from "jsonwebtoken";
 
 import { ITokenPair, ITokenPayload } from "../types";
+import {tokenConstants} from "../constants/tokenConstants";
 
 class TokenService {
   public generateTokenPair(payload: ITokenPayload): ITokenPair {
-    const accessToken = jwt.sign(payload, "JWT_ACCESS_SECRET", {
+    const accessToken = jwt.sign(payload, tokenConstants.ACCESS_SECRET, {
       expiresIn: "15m",
     });
-    const refreshToken = jwt.sign(payload, "JWT_REFRESH_SECRET", {
+    const refreshToken = jwt.sign(payload, tokenConstants.REFRESH_SECRET, {
       expiresIn: "30d",
     });
     return { accessToken, refreshToken };

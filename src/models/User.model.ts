1d7faad3965320d3652fb26ca6c4b1d202db6bbc
Admin <sorokivskyi777@gmail.com>
2023-03-29 16:40:25 +0300

    lesson 9 pagination learning

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,7 @@
 import { model, Schema } from "mongoose";
 
 import { EGenders, EUserStatus } from "../enums";
+import { IUser } from "../types";
 
 const userSchema = new Schema(
   {
@@ -37,5 +38,19 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+interface IUserMethods {
+  nameWithAge(): void;
+}
+
+userSchema.methods = {
+  nameWithAge() {},
+};
+
+userSchema.statics = {
+  async findByName(name: string): Promise<IUser[]> {
+    return this.find({ name });
+  },
+};
 
 export const User = model("user", userSchema);

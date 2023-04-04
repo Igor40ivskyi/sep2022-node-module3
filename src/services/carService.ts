4019f40577aa3ef3d2987502af77761524db9e7c
Admin <sorokivskyi777@gmail.com>
2023-04-04 14:22:42 +0300

    lesson9 resolve learning

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -1,6 +1,6 @@
 import { ApiError } from "../errors";
-import { User } from "../models";
-import { IUser } from "../types";
+import { Car } from "../models";
+import { ICar } from "../types";
 
 interface IPaginationResponse<T> {
   page: number;
@@ -18,54 +18,14 @@ export interface IQuery {
   [key: string]: string | number;
 }
 
-class UserService {
-  public async getAll(): Promise<IUser[]> {
+class CarService {
+  public async getAll(): Promise<ICar[]> {
     try {
-      return User.find();
-    } catch (e) {
-      throw new ApiError(e.message, e.status);
-    }
-  }
-
-  public async getWithPagination(
-    query: IQuery
-  ): Promise<IPaginationResponse<IUser>> {
-    try {
-      const user = await User.findById("6423e2ddf3fcf6d4d84ab077");
-
-      console.log(user.nameWithSurname);
-
-      const queryStr = JSON.stringify(query);
-      const queryObj = JSON.parse(
-        queryStr.replace(/\b(gte|lte|gt|lt)\b/, (match) => `$${match}`)
-      );
-
-      const {
-        page = 1,
-        limit = 5,
-        sortedBy = "createdAt",
-        ...searchObject
-      } = queryObj;
-
-      const skip = limit * (page - 1);
-
-      const users = await User.find(searchObject)
-        .limit(limit)
-        .skip(skip)
-        .sort(sortedBy);
-      const userstotalCount = await User.count();
-
-      return {
-        page: +page,
-        itemsCount: userstotalCount,
-        perPage: +limit,
-        itemsFound: users.length,
-        data: users,
-      };
+      return Car.find();
     } catch (e) {
       throw new ApiError(e.message, e.status);
     }
   }
 }
 
-export const userService = new UserService();
+export const carService = new CarService();

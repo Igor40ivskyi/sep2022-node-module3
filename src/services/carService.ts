d121994007fd420c9753bf6c04e582f89b8f6a9e
Admin <sorokivskyi777@gmail.com>
2023-04-04 12:03:28 +0300

    lesson9 resolve learning

---
+++ src/services/carService.ts
@@ -0,0 +1,71 @@
+import { ApiError } from "../errors";
+import { User } from "../models";
+import { IUser } from "../types";
+
+interface IPaginationResponse<T> {
+  page: number;
+  perPage: number;
+  itemsCount: number;
+  itemsFound: number;
+  data: T[];
+}
+
+export interface IQuery {
+  page: string;
+  limit: string;
+  sortedBy: string;
+
+  [key: string]: string | number;
+}
+
+class UserService {
+  public async getAll(): Promise<IUser[]> {
+    try {
+      return User.find();
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+
+  public async getWithPagination(
+    query: IQuery
+  ): Promise<IPaginationResponse<IUser>> {
+    try {
+      const user = await User.findById("6423e2ddf3fcf6d4d84ab077");
+
+      console.log(user.nameWithSurname);
+
+      const queryStr = JSON.stringify(query);
+      const queryObj = JSON.parse(
+        queryStr.replace(/\b(gte|lte|gt|lt)\b/, (match) => `$${match}`)
+      );
+
+      const {
+        page = 1,
+        limit = 5,
+        sortedBy = "createdAt",
+        ...searchObject
+      } = queryObj;
+
+      const skip = limit * (page - 1);
+
+      const users = await User.find(searchObject)
+        .limit(limit)
+        .skip(skip)
+        .sort(sortedBy);
+      const userstotalCount = await User.count();
+
+      return {
+        page: +page,
+        itemsCount: userstotalCount,
+        perPage: +limit,
+        itemsFound: users.length,
+        data: users,
+      };
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+}
+
+export const userService = new UserService();

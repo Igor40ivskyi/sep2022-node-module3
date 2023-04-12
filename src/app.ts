a8f0398137adacf52157255cef575e1967016e39
Admin <sorokivskyi777@gmail.com>
2023-04-12 17:51:35 +0300

    lesson 12 socket learning

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import * as http from "node:http";
+
 import express, { NextFunction, Request, Response } from "express";
 import fileUploader from "express-fileupload";
 import * as mongoose from "mongoose";
+import { Server } from "socket.io";
 
 import { configs } from "./configs";
 import { cronRunner } from "./crons";
@@ -9,6 +12,36 @@ import { authRouter, carRouter } from "./routers";
 import { userRouter } from "./routers";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "http://localhost:63342" } });
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("test:action", (data) => {
+    console.log(data);
+  });
+
+  socket.emit("test:message", { message: "Hello it is first server emit!" });
+
+  socket.on("send:message", (data) => {
+    // socket.broadcast.emit("broadcast:message", data);
+    // io.sockets.in(socket.id).emit("new_msg", { msg: "hello" });
+  });
+
+  socket.on("join:room1", (roomInfo) => {
+    socket.join(roomInfo.roomId);
+
+    io.to(roomInfo.roomId).emit("joined:room", "JOINED ROOM TEST MESSAGE");
+  });
+
+  socket.on("leave:room", (roomInfo) => {
+    socket.leave(roomInfo.roomId);
+
+    io.to(roomInfo.roomId).emit("left:room", "THIS USER LEFT ROOM!");
+  });
+});
 
 app.use(express.json());
 app.use(fileUploader());
@@ -28,7 +61,7 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
   await mongoose.connect(configs.DB_URL);
   cronRunner();
   console.log(`the server has started on port ${configs.PORT}`);

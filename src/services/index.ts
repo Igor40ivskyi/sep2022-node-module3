6a37aba10b52d3bbbad5f9a69ba65dd8fbf5d2e9
Admin <sorokivskyi777@gmail.com>
2023-07-28 09:46:07 +0300

    fix

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,4 @@ export * from "./carService";
 export * from "./emailService";
 export * from "./passwordService";
 export * from "./s3Service";
-export * from "./smsService";
 export * from "./tokenService";
